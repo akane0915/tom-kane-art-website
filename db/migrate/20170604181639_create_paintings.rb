3648f3fd5f156156d4aa70e811557d84eddb9a69
Spencer Alan <spencer.alan@icloud.com>
2017-06-04 11:33:02 -0700

    add paintings migration

--- db/migrate/20170604181639_create_paintings.rb
+++ db/migrate/20170604181639_create_paintings.rb
@@ -1,7 +1,16 @@
 class CreatePaintings < ActiveRecord::Migration[5.1]
   def change
-    create_table :paintings do |t|
+    enable_extension "pgcrypto"
 
+    create_table :paintings, id: :uuid do |t|
+      t.string :title
+      t.date :creation_date
+      t.string :description
+      t.string :dimensions
+      t.string :medium
+      t.string :support
+      t.boolean :framed
+      t.decimal :price, precision: 12, scale: 2
       t.timestamps
     end
   end
