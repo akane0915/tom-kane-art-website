76f9fce216e7dd04bf73c795f07fef293647ff10
Spencer Alan <spencer.alan@icloud.com>
2017-06-04 17:03:04 -0700

    add database seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,29 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Painting.create!([
+  { title: 'Man in Sky',
+    description: 'It is a man in the sky.',
+    year: '2008',
+    dimensions: '24x60',
+    medium: 'Acrylic',
+    support: 'Canvas',
+    framed: false,
+    price: '3000', },
+  { title: 'Man in Earth',
+    description: 'It is a man in the dirt.',
+    year: '2009',
+    dimensions: '24x60',
+    medium: 'Acrylic',
+    support: 'Canvas',
+    framed: false,
+    price: '3000', },
+  { title: 'Man in Lake',
+    description: 'It is a man in the lake.',
+    year: '2010',
+    dimensions: '24x60',
+    medium: 'Acrylic',
+    support: 'Canvas',
+    framed: false,
+    price: '3000', },
+])
