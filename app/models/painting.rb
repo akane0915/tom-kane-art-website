65c471fc4d769c4cd2d3c1c2072b9bd7e4942d83
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-06-11 17:36:09 -0700

    updated

--- app/models/painting.rb
+++ app/models/painting.rb
@@ -1,4 +1,2 @@
-class Painting < ApplicationRecord::Base
-  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
- validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+class Painting < ApplicationRecord
 end
