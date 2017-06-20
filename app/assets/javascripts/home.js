430f46fa21de63d3e1cffd55caf8a52749576331
Grady Shelton and Spencer Alan <grady@>
2017-06-20 15:03:54 -0700

    integrate fades with titles

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,11 +1,22 @@
 (function($) {
-    $(document).ready(function(){
-        $(window).scroll(function(){
-            if ($(this).scrollTop() > 300) {
-                $('#homepage-navbar').fadeIn(500);
-            } else {
-                $('#homepage-navbar').fadeOut(500);
-            }
-        });
+
+
+  $(document).ready(function(){
+    $(window).scroll(function(){
+      if ($(this).scrollTop() > 300) {
+        $('#homepage-navbar').fadeIn(500);
+      } else {
+        $('#homepage-navbar').fadeOut(500);
+      }
     });
+
+    $(".image-container").hover(function() {
+      $(this).find(".painting-center-title").fadeIn(300);
+    },
+    function() {
+      $(this).find(".painting-center-title").fadeOut(300);
+    });
+
+
+  });
 })(jQuery);
