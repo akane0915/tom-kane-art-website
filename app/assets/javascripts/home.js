ff1b67f1964d2bd5178cd801135db39d1acc4a16
Grady Shelton and Spencer Alan <grady@>
2017-06-20 16:08:38 -0700

    update fade speed for jumbotron

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -10,9 +10,9 @@
 
     $('<img/>').attr('src', 'https://c1.staticflickr.com/5/4245/35040805720_3ff5b9f37e_o.png').on('load', function() {
       $(this).remove();
-      $('.jumbotron').css('background-image', 'url(https://c1.staticflickr.com/5/4245/35040805720_3ff5b9f37e_o.png)').hide().fadeIn(0);
+      $('.jumbotron').css('background-image', 'url(https://c1.staticflickr.com/5/4245/35040805720_3ff5b9f37e_o.png)').hide().fadeIn(3000);
 
-      $('#overlay').fadeIn(0);
+      $('#overlay').fadeIn(5000);
     });
 
     $("#arrow").click(function() {
