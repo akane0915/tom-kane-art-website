ac1407b01d0082ec4efbb7a096dda138043bbf9f
Asia Kane and Spencer Alan <spencer@>
2017-07-10 09:59:01 -0700

    add form and button for shopping cart and update app controller with current_order helper method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,15 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   helper_method :current_admin
+  helper_method :current_order
+
+  def current_order
+    if session[:order_id]
+      Order.find(session[:order_id])
+    else
+      Order.new
+    end
+  end
 
   def after_sign_in_path_for(admins)
     paintings_path
