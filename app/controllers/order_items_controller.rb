042405f3b345a0eed9ce49354261b22201dec7c2
Grady Shelton and Jennifer Kinsey <grady@>
2017-07-10 13:30:55 -0700

    add conditional to not add multiple paintings to a cart and disallow paintings being added if not marked for sale

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -2,13 +2,12 @@ class OrderItemsController < ApplicationController
 
   def create
     @order = current_order
-    binding.pry
     @item = @order.order_items.new(item_params)
     # @item.order_id = current_order.id
     # @order.order_items.push(@item)
     @order.save
     session[:order_id] = @order.id
-    redirect_to home_path #maybe something else...
+    redirect_to cart_path #maybe something else...
   end
 
   def destroy
