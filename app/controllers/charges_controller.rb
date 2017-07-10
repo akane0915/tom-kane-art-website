e68f5bea50fc45b2dec2203bf0f3d60b23cb933f
Asia Kane and Spencer Alan <spencer@>
2017-07-10 15:18:58 -0700

    refactor stripe

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,25 +1,33 @@
 class ChargesController < ApplicationController
+  before_action :amount_to_be_charged
+  before_action :description
+
   def new
   end
 
   def create
-    # Amount in cents
-    @amount = 500
-
-    customer = Stripe::Customer.create(
-      :email => params[:stripeEmail],
-      :source  => params[:stripeToken]
+    customer = StripeTool.create_customer(
+      email: params[:stripeEmail],
+      stripe_token: params[:stripeToken]
     )
 
-    charge = Stripe::Charge.create(
-      :customer    => customer.id,
-      :amount      => @amount,
-      :description => 'Rails Stripe customer',
-      :currency    => 'usd'
+    charge = StripeTool.create_charge(
+      customer_id: customer.id,
+      amount: @amount,
+      description: @description
     )
 
   rescue Stripe::CardError => e
     flash[:error] = e.message
     redirect_to new_charge_path
   end
+private
+  def amount_to_be_charged
+    @amount = 200000
+  end
+
+  def description
+    @description = "Amazing Paintings!"
+  end
+
 end
