e641795a34c85845b6ef4fdb749a9b5ffa0626c8
Spencer Alan <spencer.alan@icloud.com>
2017-07-28 16:52:10 -0700

    fix stripe tool

--- app/models/concerns/stripe_tool.rb
+++ app/models/concerns/stripe_tool.rb
@@ -1,12 +1,12 @@
 module StripeTool
-  def self.create_customer(email: email, stripe_token: stripe_token)
+  def self.create_customer(email, stripe_token)
     Stripe::Customer.create(
       email: email,
       source: stripe_token
     )
   end
 
-  def self.create_charge(customer_id: customer_id, amount: amount, description: description)
+  def self.create_charge(customer_id, amount, description)
     Stripe::Charge.create(
       customer: customer_id,
       amount: amount,
