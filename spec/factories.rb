3260489cb590dbb27301111d7cfa5aef9f24bc5a
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-07-28 16:46:59 -0700

    adds more tests for painting model

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,9 +1,9 @@
 FactoryGirl.define do
   factory :charge do
-    
+
   end
   factory :admin do
-    
+
   end
   factory :painting do
     title 'Cherry Blossoms With Wind'
@@ -16,5 +16,6 @@ FactoryGirl.define do
     price '1500'
     image 'https://images-na.ssl-images-amazon.com/images/I/31Dy9-24ulL._SY355_.jpg'
     style 'landscape'
+    status 'for sale'
   end
 end
--- spec/models/charge_spec.rb
+++ spec/models/charge_spec.rb
@@ -10,4 +10,8 @@ RSpec.describe Charge, type: :model do
   it { should validate_presence_of :state }
   it { should validate_presence_of :zip }
   it { should validate_presence_of :country }
+
+  # describe "#calculate_total" do
+  #
+  # end
 end
