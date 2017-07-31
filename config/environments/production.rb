4636616cf10bec3be4c3b47b87603b1ece626b54
Spencer Alan <spencer.alan@icloud.com>
2017-07-31 16:18:00 -0700

    fix environment variable

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
   config.paperclip_defaults = {
     storage: :s3,
     s3_credentials: {
-      bucket: ENV.fetch('S3_BUCKET_NAME'),
+      bucket: ENV.fetch('AWS_S3_BUCKET'),
       access_key_id: ENV.fetch('AWS_ACCESS_KEY_ID'),
       secret_access_key: ENV.fetch('AWS_SECRET_ACCESS_KEY'),
       s3_region: ENV.fetch('AWS_REGION'),
