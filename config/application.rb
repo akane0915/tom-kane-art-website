7e7245008f830e1bde71b1a053e1c72efb8213ed
akane0915 <asialkane@gmail.com>
2017-08-07 15:18:57 -0700

    add config to app.rb to compile assets automatically

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,8 @@ module TomKaneArt
     config.generators do |g|
       g.orm :active_record, primary_key_type: :uuid
     end
+
+    config.serve_static_assets = true
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
