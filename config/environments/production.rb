156e9752be99d906ee3cc2db8fc351fc9eafbda9
Spencer Alan <spencer.alan@icloud.com>
2017-09-10 12:57:16 -0700

    add ssl to hardcoded images

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -93,7 +93,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   #Enabled for Devise
-  config.action_mailer.default_url_options = { host: 'http://www.tomkaneart.com' }
+  config.action_mailer.default_url_options = { host: 'https://www.tomkaneart.com' }
 
   config.action_mailer.delivery_method = :mailgun
   config.action_mailer.mailgun_settings = {
