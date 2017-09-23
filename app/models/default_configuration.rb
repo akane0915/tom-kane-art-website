32c34add35dc3dad1e79473e617ba896f863d42a
Spencer Alan <spencer.alan@icloud.com>
2017-09-23 14:15:32 -0700

    remove config callback

--- app/models/default_configuration.rb
+++ app/models/default_configuration.rb
@@ -1,15 +1,5 @@
 class DefaultConfiguration < ApplicationRecord
-  after_save :update_orders
-
   validates_numericality_of :shipping_fee,
     greater_than_or_equal_to: 0,
     allow_nil: true
-
-private
-
-  def update_orders
-    Order.find_each do |order|
-      order.save
-    end
-  end
 end
